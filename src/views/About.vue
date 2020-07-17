ff52f1cc2d83a3a6684451b8acb67e889331e7cd
Tan Nguyen <tannguyen1024@gmail.com>
2020-07-16 23:07:27 -0500

    Completed application

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList application v1.0.0.  It is part of the Vue crash course from YouTube.  Created in one hour.</p>
+  </div>
+</template>
